1d70034d5af655844551e921194815c384ff1379
miguelsacardo <miguelsacardo3@gmail.com>
2024-11-04 16:52:48 -0300

    commit somativa python 04/11/2024

--- formativo/formativa_1.py
+++ formativo/formativa_1.py
@@ -25,22 +25,41 @@ Exiba mensagens apropriadas para cada operação, como "Estoque insuficiente!" o
 #if, elif e else para controlar cada operação
 #for para representar a venda de cada unidade do produto
 
-#variáveis
+#variável de estoque
+estoque = 0
 
 #1 - ) o while mantém  o programa em execução até escolher sair
 while True:
     #escolha do usuário
-    escolhaUsuario = int(input("Escolha uma opção: [1]ADICIONAR [2]VENDER [3]VEFICIAR ESTOQUE [4]SAIR\n"))
+    escolhaUsuario = int(input("Escolha uma opção: [1]ADICIONAR [2]VENDER [3]VERIFICAR ESTOQUE [4]SAIR\n"))
 
     if escolhaUsuario == 1:
-        pass
         #adicionar produto
+        adicaoProduto = int(input("Digite a quantida que você deseja adicionar no estoque: "))
+
+        if adicaoProduto < 0:
+            print("Informe um valor válido!")
+        else:
+            print("Estoque atualizado!")
+            #adicionando produto
+            estoque += adicaoProduto
+
     elif escolhaUsuario == 2:
-        pass
-        #vender
+       if estoque == 0:
+           print("Você não tem produtos para vender!")
+       else:
+           quantidadeVenda = int(input("Quantos produtos você deseja vender?"))
+
+           if quantidadeVenda > estoque:
+               print("Você não possui essa quantia de produtos no estoque!")
+           else:
+               for i in range(quantidadeVenda):
+                   print(i)
+
+
     elif escolhaUsuario == 3:
-        pass
         #verificar estoque
+        print(f"Você possui atualmente um estoque com {estoque} produto(s)")
     elif escolhaUsuario == 4:
         print("O programa foi parado.")
         break
