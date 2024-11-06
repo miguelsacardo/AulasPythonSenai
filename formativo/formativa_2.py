7d6d52ed9795021ba27eb1154f8fa44ef3192a21
miguelsacardo <miguelsacardo3@gmail.com>
2024-11-06 10:41:03 -0300

    projeto formativo completo finalizado. O formativo_2 foi feito tentando utilizar orientação a objetos, mas ainda não tenho um conhecimebnto robusto sobre o assunto então não foi seguido as melhores práticas.

--- formativo/formativa_2.py
+++ formativo/formativa_2.py
@@ -50,54 +50,135 @@
 
 #a função mostrar saldo atual deve calcular as receitas e despesas e mostrar sempre o resultado final
 
+'''
+nível 2 - categorias para as transações
+
+o usuário deve inserir uma categoria 
+modificar a função de registro para incluir a categoria
+Adicione uma função para listar todas as transações de uma categoria específica, solicitando que o usuário insira a categoria desejada.
+'''
+
+'''
+nível 3 - Relatórios Mensais e Análise
+
+*função para exibir o total gasto e recebido em cada categoria. -> percorre as transações e soma os valores para cada categoria
+*
+'''
+
+#variáveis para o registro das transações
 lista_transacao = []
 transacoes = {}
+novoSaldo = 0
+
+#variáveis para o registro de relatórios
+relatoriosLista = []
+relatorios = {}
 
 class GerenciadorFinancas:
-    def __init__(self,tipo,valor, descricao):
-        self.saldo = 0
+    def __init__(self,saldo,tipo,valor, descricao, categoria):
+        self.saldo = saldo
         self.tipo = tipo
         self.valor = valor
         self.descricao = descricao
+        self.categoria = categoria
 
     def registrarTransacoes(self):
         transacoes['Tipo'] = self.tipo
         transacoes['Valor'] = self.valor
         transacoes['Descricao'] = self.descricao
+        transacoes['Categoria'] = self.categoria
 
+        print(f"Seu saldo depois da função é {self.saldo}")
         lista_transacao.append(transacoes.copy())
 
         return lista_transacao
 
-    def calcularSaldo(self):
-        if self.tipo == "RECEITA":
-            self.saldo = self.saldo + self.valor
-        elif self.tipo == "DESPESA":
-            self.saldo = self.saldo - self.valor
+#*criar função exibir_saldo que ao ser chamada imprime o saldo atual
+def exibir_saldo():
+    return novoSaldo
+        
+#*criar uma função listar_transações para exibir cada transação em uma linha, com informações formatadas
+def listar_transacoes():
+    for transacao in lista_transacao:
+        print (f"Tipo da transação: {transacao['Tipo']} | Valor da transação: R${transacao['Valor']} | Descrição da transação: {transacao['Descricao']} | Categoria da transação: {transacao['Categoria']}")
 
-        return self.saldo
+# função para listar todas as transações de uma categoria específica
+def listar_categorias(categoria):
+    for transacao in lista_transacao:
+        if transacao['Categoria'] == categoria:
+            print (f"Tipo da transação: {transacao['Tipo']} | Valor da transação: R${transacao['Valor']} | Descrição da transação: {transacao['Descricao']} | Categoria da transação: {transacao['Categoria']}")
+
+#função para calcular o total gasto em cada categoria
+def totalCategoria(escolherCategoria):
+    valorTotalReceitas = 0
+    valorTotalDespesas = 0
+    for transacao in lista_transacao:
+        if transacao['Categoria'] ==  escolherCategoria:
+            if transacao['Tipo'] == "RECEITA":  
+                valorTotalReceitas += transacao['Valor']
+                
+            else:
+                valorTotalDespesas += transacao['Valor']
+                
+   
+    print(f"O valor total da RECEITA da categoria {transacao['Categoria']} é {valorTotalReceitas}")
+    print(f"O valor total da DESPESA da categoria {transacao['Categoria']} é {valorTotalDespesas}")
+
+    print("\n", "-" * 50, "\n")
+
+#relatório mensal
+def relatorioMes(mes, saldo):
+    relatorios['Mes'] = mes
+    relatorios['Saldo'] = saldo
+    relatoriosLista.append(relatorios.copy())
+
+def mostrarRelatorioMes(escolherMes):
+    for relatorio in relatoriosLista:
+        if relatorio['Mes'] == escolherMes:
+            mesRelatorio = relatorio['Mes']
+            saldoReatorio = relatorio['Saldo']
+        
+    print(f"Mês do relatório: {mesRelatorio} | Saldo do mês: {saldoReatorio}")
+    
 
 while True:
 
     # *o usuário escolhe entre registrar uma receita [1] ou uma despesa [2]
-    escolhaUsuario = int(input("Escolha uma opção \n [1]Receita \n [2]Despesa \n [3]Sair \n"))
+    escolhaUsuario = int(input("Escolha uma opção \n [1]Receita \n [2]Despesa \n [3]Visualizar Saldo Atual \n [4]Verificar Transações \n [5]Filtrar Registros por Categoria \n [6]Relatório e análise Mensal ou por Categoria \n [7]Sair \n"))
 
     match escolhaUsuario:
         case(1):
+            #verificando se o saldo está negativo
+            if novoSaldo < 0:
+                print("\n", "-" * 50, "\n")
+
+                print("Seu saldo está negativo! A receita será usada para cobrir sua dívida!")
+            
+                print("\n", "-" * 50, "\n")
+
             print("Você escolheu registrar uma RECEITA")
 
             #solicitar os dados do usuário
             registroTipo = "RECEITA"
             registroValor = float(input("Informe o valor da sua receita: \n"))
             registroDesricao = str(input("Escreva a descrição do seu registro: \n"))
+            registroCategoria = str(input("Escreva a categoria (onde você gerou sua receita?): \n")).upper()
+            regitroMes = str(input("Em que mês foi registrado essa receita? (Digite em formato 00) \n"))
 
             print("\n", "-" * 50, "\n")
 
-            print(f"Tipo de registro: {registroTipo} \n Valor: {registroValor} \n Descricao: {registroDesricao} \n")
+            print(f"Tipo de registro: {registroTipo} \n Valor: {registroValor} \n Descricao: {registroDesricao} \n Categoria: {registroCategoria} \n")
             enviarEscolha = str(input("Você confirma o salvamento das informações acima? [S/N] \n")).upper()[0]
 
             if enviarEscolha == "S":
-                gerenciamento_fincanceiro = GerenciadorFinancas(registroTipo, registroValor, registroDesricao)
+                #o saldo do usuário é somado de acordo com as receitas
+                novoSaldo += registroValor 
+
+                #registra no relatório mensal
+                relatorioMes(regitroMes, novoSaldo)
+
+                #instancia a classe
+                gerenciamento_fincanceiro = GerenciadorFinancas(novoSaldo, registroTipo, registroValor, registroDesricao, registroCategoria)
                 print(f"Registro da sua transação feita com sucesso!")
 
                 print("\n", "-" * 50, "\n")
@@ -105,29 +186,49 @@ while True:
                 #chama a função de registrar os dados
                 gerenciamento_fincanceiro.registrarTransacoes()
 
-                #chama a função de calcular o saldo
-                print(gerenciamento_fincanceiro.calcularSaldo())
-
             else:
                 print(f"Seu registro não foi feito.")
 
                 print("\n", "-" * 50, "\n")
 
         case(2):
+            #verificando se o saldo está negativo OU IGUAL A 0
+            if novoSaldo == 0:
+                print("\n", "-" * 50, "\n")
+
+                print("Seu saldo irá ficar negativo!")
+
+                print("\n", "-" * 50, "\n")
+
+            elif novoSaldo < 0:
+                print("\n", "-" * 50, "\n")
+
+                print("Seu saldo está negativo! O valor de dívida será aumentado!")
+
+                print("\n", "-" * 50, "\n")
+
             print("Você escolheu registrar uma DESPESA")
 
             #solicitar dados ao usuário
             registroTipo = "DESPESA"
             registroValor = float(input("Informe o valor da sua despesa: \n"))
             registroDesricao = str(input("Escreva a descrição do seu registro: \n"))
+            registroCategoria = str(input("Escreva a categoria (onde você gerou sua receita?): ")).upper()
+            regitroMes = str(input("Em que mês foi registrado essa despesa? (Digite em formato 00) \n"))
 
             print("\n", "-" * 50, "\n")
 
-            print(f"Tipo de registro: {registroTipo} \n Valor: {registroValor} \n Descricao: {registroDesricao} \n")
+            print(f"Tipo de registro: {registroTipo} \n Valor: {registroValor} \n Descricao: {registroDesricao} \n Categoria: {registroCategoria} \n")
             enviarEscolha = str(input("Você confirma o salvamento das informações acima? [S/N] \n")).upper()[0]
 
             if enviarEscolha == "S":
-                gerenciamento_fincanceiro = GerenciadorFinancas(registroTipo, registroValor, registroDesricao)
+                #o saldo do usuário é decrementado de acordo com as despesas
+                novoSaldo -= registroValor
+                
+                #registra no relatório mensal
+                relatorioMes(regitroMes, novoSaldo)
+
+                gerenciamento_fincanceiro = GerenciadorFinancas(novoSaldo, registroTipo, registroValor, registroDesricao, registroCategoria)
                 print(f"Registro da sua transação feita com sucesso!")
 
                 print("\n", "-" * 50, "\n")
@@ -135,24 +236,66 @@ while True:
                 # chama a função de registrar os dados
                 gerenciamento_fincanceiro.registrarTransacoes()
 
-                # chama a função de calcular o saldo
-                print(gerenciamento_fincanceiro.calcularSaldo())
-
             else:
                 print(f"Seu registro não foi feito.")
 
                 print("\n", "-" * 50, "\n")
-
+        
+        # 2 - Visualizar saldo atual
         case(3):
-            pass
+            print("\n", "-" * 50, "\n")
 
-# tipoT = str(input("Informe o tipo"))
-# valorT = float(input("Informe  valor"))
-# descricaoT = str(input("Digite uma descrição para a transição"))
-# gerenciar = GerenciadorFinancas(tipoT, valorT, descricaoT)
-#
-#
-# print(gerenciar.registrarTransacoes())
+            print(f"Seu saldo atual é R${exibir_saldo()}")
+
+            if novoSaldo < 0:
+                print("\nVocê está devendo! Gere receitas para cobrir sua dívida!")
+
+            print("\n", "-" * 50, "\n")
+        
+        #3 - Listar transações
+        case(4):
+            print("\n", "-" * 50, "\n")
+
+            listar_transacoes()
+
+            print("\n", "-" * 50, "\n")
+        
+        #o usuário deve inserir uma categoria
+        case(5):
+            print("\n", "-" * 50, "\n")
+
+            filtrarCategoria = str(input("Pesquise uma categoria para filtrar as transações: ")).upper()
+            listar_categorias(filtrarCategoria)
+            
+            print("\n", "-" * 50, "\n")
+        
+        #calcula totais por categoria e saldo mensal
+        case(6):
+
+            filtroEscolha = str(input("Filtrar por mês [M] ou filtrar por categoria [C]? \n")).upper()[0]
+
+            if filtroEscolha == "C":
+                print("\n", "-" * 50, "\n")
+                escolherCategoria = str(input("Insira a categoria que você deseja calcular o total de todas as transações: \n")).upper()
+                print("\n", "-" * 50, "\n")
+                totalCategoria(escolherCategoria)
+            elif filtroEscolha == "M":  
+                print("\n", "-" * 50, "\n")
+                escolherMes = str(input("Que mês você deseja consultar? \n")).upper()
+                print(f"No mês {escolherMes} você obteve: \n")
+                mostrarRelatorioMes(escolherMes)
+                print("\n", "-" * 50, "\n")
+
+        #sair do programa
+        case(7):
+            print("A execução do programa parou!")
+            break
+
+        #caso nenhuma das opções for selecionada
+        case(_):
+            print("Selecione uma opção válida!")
+        
+
 
 
 
